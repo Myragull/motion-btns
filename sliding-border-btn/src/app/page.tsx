2dc3c2d0ab878c3d8743827cc7ad588e84a627d8
Myra Gull <myragull749@gmail.com>
2026-03-15 14:28:33 +0500

    added sliding border btn

--- sliding-border-btn/src/app/page.tsx
+++ sliding-border-btn/src/app/page.tsx
@@ -8,7 +8,12 @@ export default function Home() {
     Get Started
    </button>
 
-   
+  <button className="overflow-hidden relative group bg-[#e6e6e6] text-gray-950 px-4 py-2 font-medium rounded-md hover:text-black hover:bg-white transition duration-300 ease-in-out cursor-pointer">
+    Get Started
+    <span className="absolute left-0 bottom-0 h-full w-[0px] bg-red-400 transition-all duration-300 group-hover:w-[2px]"></span>
+     <span className="absolute right-0 bottom-0 h-full w-[0px] bg-green-400 transition-all duration-300 group-hover:w-[2px]"></span>
+   </button>
+
    </div>
   );
 }
