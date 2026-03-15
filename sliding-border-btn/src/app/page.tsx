be0571ef94c42087ae4a3892a0d23efaf2efcbea
Myra Gull <myragull749@gmail.com>
2026-03-15 14:24:50 +0500

    build simple over button

---
+++ sliding-border-btn/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+   <div 
+   className="px-12 py-4 bg-black h-screen flex items-center justify-center gap-10"
+   >
+   <button className="bg-[#e6e6e6] text-gray-950 px-4 py-2 font-medium rounded-md hover:text-black hover:bg-white transition duration-300 ease-in-out cursor-pointer">
+    Get Started
+   </button>
+
+   
+   </div>
+  );
+}
